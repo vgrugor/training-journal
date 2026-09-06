ccbcb31e319bc86d128410a001bc6b0d86c08607
vgrugor <42821194+vgrugor@users.noreply.github.com>
2026-09-06 12:37:05 +0300

    Cache technical progress support

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "personal-day-journal-v73";
+const CACHE_NAME = "personal-day-journal-v74";
 const ASSETS = [
   "./",
   "./index.html",
@@ -11,6 +11,7 @@ const ASSETS = [
   "./src/dictionary-edit.js",
   "./src/mobile-nav.js",
   "./src/progress.js",
+  "./src/technical-progress.js",
   "./src/styles.css",
   "./src/mobile-nav.css",
   "./icons/icon.svg"
